b0cb316a2099399e7f7b9645e5be4ad8e434c06f
juttayaya <jirawat.uttayaya@rocketinsights.com>
2021-12-19 02:49:33 -0500

    Update minimum tested TF versions for modules

--- modules/simple-vpc/providers.tf
+++ modules/simple-vpc/providers.tf
@@ -1,4 +1,7 @@
+# This is the minimum Terraform versions that this module was tested against
 terraform {
+  required_version = ">= 1.1"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
