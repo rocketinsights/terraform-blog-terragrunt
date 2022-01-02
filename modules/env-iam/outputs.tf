c95e9b8e6fbfeac5dab01a7ea09e0da209ddc9e1
juttayaya <jirawat.uttayaya@rocketinsights.com>
2022-01-02 01:13:44 -0500

    ec2_instance_profile outut arn

--- modules/env-iam/outputs.tf
+++ modules/env-iam/outputs.tf
@@ -5,3 +5,7 @@ output "ec2_role_id" {
 output "ec2_role_arn" {
   value = aws_iam_role.ec2_role.arn
 }
+
+output "ec2_instance_profile_arn" {
+  value = aws_iam_instance_profile.ec2_instance_profile.arn
+}
